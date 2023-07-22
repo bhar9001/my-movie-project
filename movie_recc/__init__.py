35fb24b276de577a90846304371525fcea744080
Bharat Suthar <103443194+bhar9001@users.noreply.github.com>
2023-07-23 00:58:04 +0530

    Update __init__.py
    by env file

--- movie_recc/__init__.py
+++ movie_recc/__init__.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://bharat:6i7pGJwI9JI2rfzx@cluster0.kumjka8.mongodb.net/movie")
-    app.db = client.get_default_database()
+    app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
     app.config["SECRET_KEY"] = os.environ.get(
         "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
     )
+    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
 
     app.register_blueprint(pages)
     return app
